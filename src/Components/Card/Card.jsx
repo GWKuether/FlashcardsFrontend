388354654eb869dad3c5611acc04e59384de35ec
George Kuether <gwkuether@gmail.com>
2022-12-16 10:02:24 -0600

    Created a Functional 'Previous' Button to Return to the Previous Card

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -6,6 +6,16 @@ const Card = (props) => {
     const [displayToggle, setDisplayToggle] = useState(true)
     const [index, setIndex] = useState(0)
 
+    
+    function handlePrevious(){
+        setIndex(index - 1)
+        if (index < 0){
+            setIndex(props.cards[-1].id)
+        }
+        setDisplayToggle(true)
+    }
+    
+    
     function handleNext(){
         setIndex(index + 1)
         setDisplayToggle(true)
@@ -18,6 +28,7 @@ const Card = (props) => {
                 <div onClick={() => setDisplayToggle(!displayToggle)}>
                 {displayToggle ? <h2>{props.cards[index]?.word}</h2> : <h4>{props.cards[index]?.definition}</h4>}
                 </div>
+                <button onClick={() => handlePrevious()}>PREVIOUS</button>
                 <button onClick={() => handleNext()}>NEXT</button>
             </div>
         </div>
